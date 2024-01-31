fb3cad5ba819e592c1e9a61a74d679253de1f7c8
Verminas <veremeeva96@gmail.com>
2024-01-31 20:04:57 +0300

    fix: bug with solution for task 3 js

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,8 +1,9 @@
 let number = 1;
-console.log(number--);
-console.log(++number + ++number);
+
+console.log(number++);
 console.log(++number);
-console.log(--number - --number);
+console.log(number--);
+console.log(--number);
 
 
 
